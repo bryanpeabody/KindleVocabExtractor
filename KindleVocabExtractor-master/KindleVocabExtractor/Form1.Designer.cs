e43e278e404efb1b6837f094bbc5e45335f1bb7e
Bryan Peabody <bryan@bryanpeabody.net>
2016-07-06 14:23:35 -0400

    Initial add

---
+++ KindleVocabExtractor-master/KindleVocabExtractor/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace KindleVocabExtractor
+{
+    partial class frmKVE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmKVE));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openVocabdbToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkForUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Selected = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Book = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(721, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openVocabdbToolStripMenuItem,
+            this.exportToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openVocabdbToolStripMenuItem
+            // 
+            this.openVocabdbToolStripMenuItem.Name = "openVocabdbToolStripMenuItem";
+            this.openVocabdbToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.openVocabdbToolStripMenuItem.Text = "Open Vocab.db";
+            this.openVocabdbToolStripMenuItem.Click += new System.EventHandler(this.openVocabdbToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkForUpdatesToolStripMenuItem,
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // checkForUpdatesToolStripMenuItem
+            // 
+            this.checkForUpdatesToolStripMenuItem.Name = "checkForUpdatesToolStripMenuItem";
+            this.checkForUpdatesToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.checkForUpdatesToolStripMenuItem.Text = "Check for updates";
+            this.checkForUpdatesToolStripMenuItem.Click += new System.EventHandler(this.checkForUpdatesToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(170, 22);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Sqlite (*.db)|*.db";
+            this.openFileDialog1.Title = "Open Sqlite file";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Selected,
+            this.Book,
+            this.id});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(721, 466);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Selected
+            // 
+            this.Selected.FalseValue = "0";
+            this.Selected.HeaderText = "";
+            this.Selected.Name = "Selected";
+            this.Selected.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Selected.TrueValue = "1";
+            this.Selected.Width = 25;
+            // 
+            // Book
+            // 
+            this.Book.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Book.HeaderText = "Book";
+            this.Book.Name = "Book";
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.Visible = false;
+            // 
+            // frmKVE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 490);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmKVE";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kindle Vocab Extractor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openVocabdbToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkForUpdatesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Selected;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Book;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+    }
+}
+
---
+++ KindleVocabExtractor-master/KindleVocabExtractor/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Data.SQLite;
+using System.Net;
+using System.Configuration;
+
+namespace KindleVocabExtractor
+{
+    public partial class frmKVE : Form
+    {
+        public frmKVE()
+        {
+            InitializeComponent();
+        }
+
+        #region Events
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openVocabdbToolStripMenuItem_Click(object sender, EventArgs e)
+        {                        
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                populateGrid();
+            }
+
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount == 0)
+            {
+                MessageBox.Show("There is nothing to export.", "Export Warning");
+            }
+            else
+            {
+                // The last checkbox checked needs to be commited.
+                dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);
+
+                Dictionary<string, string> checkedIds = GetCheckedIds();
+
+                if (checkedIds.Count == 0)
+                {
+                    MessageBox.Show("Please select at least one book before exporting.", "Export Warning");
+                    return;
+                }
+
+                bool isError = writeClozeFile(checkedIds);
+
+                if (!isError)
+                {
+                    MessageBox.Show("Finished. Your file(s) are on the desktop.", "Success");
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {            
+            StringBuilder version = new StringBuilder();
+            version.Append("Kindle Vocab Extractor").Append(Environment.NewLine);
+            version.Append("Version: ").Append(getVersionNumber()).Append(Environment.NewLine);
+            version.Append("Written by: Bryan Peabody, 2015").Append(Environment.NewLine).Append(Environment.NewLine);
+            version.Append("Visit http://www.bryanpeabody.net for more information.").Append(Environment.NewLine);
+
+            MessageBox.Show(version.ToString(), "About");
+        }
+
+        private void checkForUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            checkForUpdates();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Show a list of books in the sqlite db.
+        /// </summary>
+        private void populateGrid()
+        {            
+            using (SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + openFileDialog1.FileName + ";Version=3;"))
+            {
+                try
+                {
+                    m_dbConnection.Open();
+
+                    string sql = "select id, title  from book_info order by authors";
+
+                    SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                    SQLiteDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {                        
+                        string id = reader[0].ToString();
+                        string title = reader[1].ToString();
+
+                        var index = dataGridView1.Rows.Add();
+                        dataGridView1.Rows[index].Cells["Book"].Value = title;
+                        dataGridView1.Rows[index].Cells["id"].Value = id;
+                    }
+                                                           
+                    // Resize the DataGridView columns to fit the newly loaded content.
+                    dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+                    
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("There was an error reading the SQLite database: " + e.Message);
+                }
+                finally
+                {
+                    m_dbConnection.Close();                    
+                }
+            }
+        }
+
+        /// <summary>
+        /// Dictionary is id, book title.
+        /// </summary>
+        /// <returns></returns>
+        private Dictionary<string, string> GetCheckedIds()
+        {
+            Dictionary<string, string> checkedIds = new Dictionary<string, string>();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["Selected"].Value != null)
+                {
+                    if (int.Parse(row.Cells["Selected"].Value.ToString()) == 1)
+                    {
+                        checkedIds.Add(row.Cells["id"].Value.ToString(), row.Cells["Book"].Value.ToString());
+                    }
+                }
+            }
+
+            return checkedIds;
+        }
+
+        /// <summary>
+        /// For each book, get a list of the vocab and example sentences. Then write out a file with the cloze cards for each book.
+        /// </summary>
+        /// <param name="bookIDs"></param>
+        /// <returns></returns>
+        private bool writeClozeFile(Dictionary<string, string> bookIDs)
+        {
+            bool isError = false;
+
+            using (SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + openFileDialog1.FileName + ";Version=3;"))
+            {
+                try
+                {
+                    m_dbConnection.Open();
+
+                    foreach (string bookID in bookIDs.Keys)
+                    {
+                        string sql = "select word_key, usage from lookups where book_key = '" + bookID + "'";
+
+                        SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                        SQLiteDataReader reader = command.ExecuteReader();
+
+                        string outfile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + validateFileName(bookIDs[bookID]) + ".txt";
+                        
+                        StringBuilder sb = new StringBuilder();
+
+                        while (reader.Read())
+                        {
+                            string word = cleanWord(reader[0].ToString());
+                            string sentence = createClozeSentence(word, reader[1].ToString());
+
+                            sb.Append(sentence).Append(Environment.NewLine);
+                        }
+
+                        File.WriteAllText(outfile, sb.ToString());
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("There was an exception writing the file: " + e.Message);
+                    isError = true;
+                }
+                finally
+                {
+                    m_dbConnection.Close();
+                }
+            }
+
+            return isError;
+        }
+
+        /// <summary>
+        /// Make sure the filename doesn't have any special characters.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private static string validateFileName(string name)
+        {
+            foreach (var c in Path.GetInvalidFileNameChars())
+            {
+                name = name.Replace(c, '_');
+            }
+
+            return name;
+        }
+
+        /// <summary>
+        /// Incoming format is like this -> es:hola. Strip out the en: and just leave the actual word.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        private string cleanWord(string word)
+        {            
+           if (!string.IsNullOrEmpty(word))
+            {
+                word = word.Substring(word.LastIndexOf(':') + 1);
+            }
+
+           return word;
+        }
+
+        /// <summary>
+        /// Create the cloze card version of each entry.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="sentence"></param>
+        /// <returns></returns>
+        private string createClozeSentence(string word, string sentence)
+        {            
+            if (!string.IsNullOrEmpty(word) && !string.IsNullOrEmpty(sentence))
+            {
+                sentence = sentence.Replace(word, "{{c1::" + word + "}}");
+            }
+
+            return sentence;
+        }
+
+        /// <summary>
+        /// Gets the current version number.
+        /// </summary>
+        /// <returns></returns>
+        private string getVersionNumber()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return fileVersionInfo.ProductVersion;
+        }
+
+        /// <summary>
+        /// Check for a new version.
+        /// </summary>
+        private void checkForUpdates()
+        {
+            bool hasUpdate = false;
+            string url = "http://bryanpeabody.net/versions/KVE/KVE.txt";
+            string clientVersion = getVersionNumber();
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    string hostVersion = client.DownloadString(url);
+
+                    if (hostVersion.Trim() != clientVersion.Trim())
+                    {
+                        hasUpdate = true;
+                    }
+                }
+                catch (Exception e)
+                { 
+                    // Just ignore 
+                }
+            }
+
+            if (hasUpdate)
+            {
+                if (MessageBox.Show("There is an update available!" + Environment.NewLine + "Would you like to download it?", "Update software", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    System.Diagnostics.Process.Start("http://bryanpeabody.net/kindle-vocab-extractor/"); 
+                }
+            }  
+            else
+            {
+                MessageBox.Show("Kindle Vocab Extractor is up to date.", "Update software");
+            }
+        }             
+    }
+}
